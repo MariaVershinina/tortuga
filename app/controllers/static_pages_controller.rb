5b50f3919084a262dc3aab8e7d1c0d96c0facdb2
Maria Vershinina <mariavershina@gmail.com>
2016-10-13 13:05:19 +0300

    New

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contacts
+  end
+
+  def photo
+  end
+
+  def friends
+  end
+
+  def konkursy
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,26 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+TortugaPark::Application.config.secret_key_base = 'e3d9f6c4311f59af2d649a7abaf670999f35db7a1165ac8b4fe00cec3c960c7f1863d4fb5454cdad96272700f981e80acc6b7db1855929293002de1abf06c1ba'
